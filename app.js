c09d62cb4646c9041c4cec67f0f508849f52b78d
pimlu <pimlu@users.noreply.github.com>
2016-01-01 18:55:15 -0500

    add request/game maintenance, reusable templates, clicking
    so now both interaction and rendering are proven to work.  this is
    doable.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 const http = require('http'),
-  fs = require('fs');
+  fs = require('fs'),
+  _ = require('lodash'),
+  uuid = require('node-uuid');
 
 const hostname = '127.0.0.1';
 const port = 1337;
@@ -12,7 +14,7 @@ const header = fs.readFileSync('./header.html', 'utf8') + gentable();
 function gentable() {
   let out = '<div class="board">';
   for(let x=0; x<width; x++) {
-    out += '<div class="move'+x+'">';
+    out += `<div class="move${x}">`;
     for(let y=0; y<height; y++) {
       out += '<div></div>';
     }
@@ -22,15 +24,35 @@ function gentable() {
   return out;
 }
 
+const colors = ['red', 'yel'];
+function css(str) {
+  return `<style>${str}</style>`;
+}
+//these all get sent multiple times as the game plays out
+const templates = {
+  puck: (color, x, y) => `<div class="puck ${color}" style="left:${x}00px;top:${y}00px"></div>`,
+  turn: (num) => css(`
+.turn.${colors[num%2]} {display: block;}
+.turn.${colors[num%2]} {display: none;}
+`),
+  movurl: (index, id, hash) => css(`
+.move${index}:active {
+  background: url('/move/${id}/${index}/${hash}');
+}
+`)
+}
+
 
 const actions = {
   play: play,
-  menu: menu,
   move: move
 };
 
+const games = {};
+const reqs = {};
+
 http.createServer((req, res) => {
-  console.log('GET %s', req.url);
+  console.log(`GET ${req.url}`);
   //basic routing of the form /<action>/<param1>/<param2>/<etc
   if(req.url === '/') return play(req, res, []);
   const match = /^\/([a-z]+)\/(.*)$/.exec(req.url);
@@ -51,18 +73,76 @@ function nf404(req, res) {
 }
 
 function play(req, res, args) {
+  
+  //now use our game id if we have one
+  let gid = args[0];
+  if(!gid) {
+    gid = uuid.v4();
+    games[gid] = {
+      players: [],
+      board: [],
+      turn: 0,
+      active: false
+    }
+  }
+  const game = games[gid];
+  
+  //store a UUID we associate with the request
+  const id = uuid.v4();
+  reqs[id] = {
+    req: req,
+    res: res,
+    gid: gid,
+    movhash: _.fill(Array(width), 0)
+  };
+  req.on('finish', () => {
+    //remove them from the games and active requests
+    game.players = _.remove(game.players, id);
+    if(!game.players.length) {
+      delete games[gid];
+    }
+    delete reqs[id];
+  });
+  game.players.push(id);
+  
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(header);
-  setTimeout(() => {
-    res.write('<div class="puck red" style="position:absolute;top:500px;left:0;"></div>');
-    res.end('</div></div></body></html>')
-  }, 2000);
-}
-function menu(req, res, args) {
-  res.writeHead(204, {});
-  res.end();
+  for(let i=0; i<width; i++) {
+    domovurl(id, i);
+  }
+  //res.end('</div></div></body></html>');
 }
 function move(req, res, args) {
+  //give an empty response
   res.writeHead(204, {});
   res.end();
+  
+  const id = args[0], index = +args[1];
+  if(!id || !reqs[id]) return;
+  console.log(index);
+  if(!(index>=0 && index<width)) return;
+  
+  //TODO add puck
+  
+  //wait to add a new url, cause otherwise if the mouse is held down it spams requests
+  setTimeout(() => {
+    if(reqs[id].finished) return; //don't throw an error if they quit or something
+    domovurl(id, index);
+  }, 500);
+}
+//send them a new move url so they can click again
+//the reason why I do this is because in my tests telling it to expire/not to cache didn't work
+function domovurl(id, index) {
+  const req = reqs[id];
+  const hash = req.movhash[index]++;
+  req.res.write(templates.movurl(index,id,hash));
+}
+function broadcast(gid, text) {
+  const game = games[gid];
+  Object.keys(game.players).forEach(send.bind(null, gid, text));
+}
+function send(gid, text, pid) {
+  const game = games[gid];
+  const res = reqs[game.players[pid]].res;
+  res.write(text);
 }
